e603e9a96d1a3d99bb85aec75b886dca13c4f42d
ZEROX <ramafebrian520@gmail.com>
2024-11-08 00:29:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    // Cek username dan password (ini hanya contoh, seharusnya dilakukan dengan backend)
+    if (username === 'zerox santana' && password === 'telestia') {
+        // Jika login berhasil, arahkan ke halaman berikutnya
+        window.location.href = 'home.html'; // Ganti dengan halaman tujuan
+    } else {
+        errorMessage.textContent = 'Username atau Password salah!';
+    }
+});
